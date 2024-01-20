8a8ed8a5414808662e31924e1f3bfabdad30e90a
aurrix <arurunov@gmail.com>
2024-01-20 20:07:42 +0100

    feat: sw * use service workers for updates * fix icon background * fix button margins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {ApplicationRef, Component, inject, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {Router, RouterOutlet} from '@angular/router';
+import {ActivatedRoute, Router, RouterOutlet} from '@angular/router';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 import {MatCardModule} from "@angular/material/card";
 import {AddWordComponent} from "./components/add-word/add-word.component";
 import {AnswerComponent} from "./components/answer/answer.component";
+import {SwUpdate, VersionReadyEvent} from "@angular/service-worker";
+import {concat, filter, first, interval} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -23,7 +25,7 @@ import {AnswerComponent} from "./components/answer/answer.component";
         <app-add-word class="ml-3"/>
         <button
           mat-raised-button
-          color="primary" class="ml-3" (click)="toggleHistory()">
+          color="primary" class="mx-3" (click)="toggleHistory()">
           @if (router.url === '/history') {
             <mat-icon>arrow_back</mat-icon>
           } @else {
@@ -38,8 +40,26 @@ import {AnswerComponent} from "./components/answer/answer.component";
   `,
   styles: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   router = inject(Router);
+  swu = inject(SwUpdate);
+  appRef = inject(ApplicationRef);
+
+  ngOnInit() {
+    if (this.swu.isEnabled) {
+      this.appRef.isStable
+        .pipe(first(isStable => isStable))
+        .subscribe(async () => {
+          await this.swu.checkForUpdate();
+        });
+      this.swu.versionUpdates
+        .pipe(filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'))
+        .subscribe(evt => {
+          alert('New version available. Application will now reload.');
+          document.location.reload();
+        });
+    }
+  }
 
   toggleSidenav() {
     if (this.router.url !== '/words') {
